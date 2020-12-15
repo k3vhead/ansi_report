76f4e3c30de3600e881c5cdb8f73c54877af30f8
DC Lewis <dclewis@thewebthing.com>
2020-12-15 05:45:04 -0600

    Report Test update 	modified:   test/com/ansi/scilla/report/test/DavesReportTester.java

--- test/com/ansi/scilla/report/test/DavesReportTester.java
+++ test/com/ansi/scilla/report/test/DavesReportTester.java
@@ -23,13 +23,13 @@ public class DavesReportTester extends AbstractReportTester {
 	public void go() throws Exception {
 		boolean makePDF = true;
 		boolean makeHTML = false;
-		boolean makeXLS = false;
+		boolean makeXLS = true;
 
 		Integer divisionId = 105;
 		Integer month = Calendar.OCTOBER;
 		Integer year = 2020;
-		Calendar startDate = new GregorianCalendar(2020, Calendar.JANUARY, 1);
-		Calendar endDate = new GregorianCalendar(2021, Calendar.DECEMBER, 1);
+		Calendar startDate = new GregorianCalendar(2020, Calendar.OCTOBER, 1);
+		Calendar endDate = new GregorianCalendar(2020, Calendar.DECEMBER, 1);
 
 //		MakeLiftAndGenie prodLiftAndGenie = new MakeLiftAndGenie( makeXLS, makePDF, makeHTML, startDate, endDate);
 //		prodLiftAndGenie.setReportConn(ReportConn.PROD);
@@ -45,13 +45,13 @@ public class DavesReportTester extends AbstractReportTester {
 //				new MakeCRRDetail(makeXLS, makePDF, makeHTML, startDate, endDate),		// this is a standard report with subtotals
 //				new MakeCRRSummary(makeXLS, makePDF, makeHTML, startDate, endDate),   		// this is a standard summary
 //				new MakeDO(makeXLS, makePDF, makeHTML, divisionId, endDate),					// this is a standard report with banner notes
-				new MakeExpiringDocumentReport(makeXLS, makePDF, makeHTML, startDate, endDate),
+//				new MakeExpiringDocumentReport(makeXLS, makePDF, makeHTML, startDate, endDate),
 //				new MakeInvoiceRegister(makeXLS, makePDF, makeHTML, divisionId, month, year),   	// this is a standard report with totals
 //				new MakeLiftAndGenieDSum(makeXLS, makePDF, makeHTML, startDate, endDate),
 //				new MakeLiftAndGenieDetailReport(makeXLS, makePDF, makeHTML, startDate, endDate),
 //				new MakeLiftAndGenie(makeXLS, makePDF, makeHTML, startDate, endDate),
 //				new MakeMonthlyServiceTaxReport(makeXLS, makePDF, makeHTML, startDate, endDate),
-//				new MakeServiceTaxByDayReport(makeXLS, makePDF, makeHTML, startDate, endDate),
+				new MakeServiceTaxByDayReport(makeXLS, makePDF, makeHTML, startDate, endDate),
 //				new MakePACDetail("C", makeXLS, makePDF, makeHTML, divisionId, startDate, endDate),			// this is a compound report
 //				new MakePACListing(makeXLS, makePDF, makeHTML, divisionId, startDate, endDate),			// this is a compound report
 //				new MakePACSummary(makeXLS, makePDF, makeHTML, divisionId, startDate, endDate),			// this is a standard report (includd in PAC Listing)
