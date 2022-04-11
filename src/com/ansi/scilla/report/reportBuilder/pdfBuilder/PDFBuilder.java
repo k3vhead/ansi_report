5453b0d720bfe0899e421d4f11361265e8150ea1
DC Lewis <dclewis@thewebthing.com>
2022-04-11 07:41:00 -0500

    Resolve merge weirdness 	modified:   scripts/build.xml 	modified:   src/com/ansi/scilla/report/reportBuilder/pdfBuilder/PDFBuilder.java

--- src/com/ansi/scilla/report/reportBuilder/pdfBuilder/PDFBuilder.java
+++ src/com/ansi/scilla/report/reportBuilder/pdfBuilder/PDFBuilder.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.commons.*;
 
 import com.ansi.scilla.report.reportBuilder.common.ColumnHeader;
 import com.ansi.scilla.report.reportBuilder.common.CustomCell;
